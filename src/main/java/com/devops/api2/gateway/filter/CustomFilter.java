ee7daa183ee6156882fc3c24c4b525392834f67e
root <sejin307@kakao.com>
2023-07-17 14:16:20 +0900

    230717

--- src/main/java/com/devops/api2/gateway/filter/CustomFilter.java
+++ src/main/java/com/devops/api2/gateway/filter/CustomFilter.java
@@ -1,12 +1,8 @@
 package com.devops.api2.gateway.filter;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 /**
