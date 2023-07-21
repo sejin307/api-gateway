625373dc99d7f093d504e8213f3558bb85941f55
sejinkim <sjkim@goodcen.com>
2023-07-21 15:32:30 +0900

    230721

--- src/main/java/com/devops/api2/gateway/config/CacheConfig.java
+++ src/main/java/com/devops/api2/gateway/config/CacheConfig.java
@@ -6,10 +6,11 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 /**
  * TODO: 기존과 동일한 요청은 CACHE처리
  */
+@Configuration
 public class CacheConfig {
 
     @Bean
