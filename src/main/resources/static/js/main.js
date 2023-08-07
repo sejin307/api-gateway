f9f252cbda46da76708e9cc7f5bb999e19267d0e
root <sejin307@kakao.com>
2023-08-08 00:59:30 +0900

    230808

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,29 +1,78 @@
-new Vue({
-    el: '#app',
-    data: {
-        username: '',
-        password: ''
-    },
-    methods: {
-        async submitForm() {
-            if(this.username && this.password) {
-                try {
-                    const response = await axios.post('/authenticate', {
-                        username: this.username,
-                        password: this.password
-                    });
-                    if (response.status === 200) {
-                        const id_token = response.data.id_token;
-                        sessionStorage.setItem('id_token', id_token);
-                        window.location.href = '/main';
-                    }
-                } catch(error) {
-                    console.log(error);
-                    alert('사용자 인증실패');
+$(function () {
+    // VARIABLES =============================================================
+    var TOKEN_KEY = "jwtToken"
+    var $login = $("#login");
+
+    // FUNCTIONS =============================================================
+    function getJwtToken() {
+        return localStorage.getItem(TOKEN_KEY);
+    }
+
+    function setJwtToken(token) {
+        localStorage.setItem(TOKEN_KEY, token);
+    }
+
+    function removeJwtToken() {
+        localStorage.removeItem(TOKEN_KEY);
+    }
+
+    function doLogin(loginData) {
+        $.ajax({
+            url: "/api/authenticate",
+            type: "POST",
+            data: JSON.stringify(loginData),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data, textStatus, jqXHR) {
+                setJwtToken(data.id_token);
+                $login.hide();
+                $notLoggedIn.hide();
+                showTokenInformation()
+                showUserInformation();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status === 401) {
+                    $('#loginErrorModal')
+                        .modal("show")
+                        .find(".modal-body")
+                        .empty()
+                        .html("<p>" + jqXHR.responseJSON.message + "</p>");
+                } else {
+                    throw new Error("an unexpected error occured: " + errorThrown);
                 }
-            } else {
-                console.log("Please fill both fields");
             }
+        });
+    }
+
+    function doLogout() {
+        removeJwtToken();
+        $login.show();
+        $userInfo
+            .hide()
+            .find("#userInfoBody").empty();
+        $loggedIn
+            .hide()
+            .attr("title", "")
+            .empty();
+        $notLoggedIn.show();
+    }
+
+    function createAuthorizationTokenHeader() {
+        var token = getJwtToken();
+        if (token) {
+            return {"Authorization": "Bearer " + token};
+        } else {
+            return {};
         }
     }
+
+    $("#loginForm").submit(function (event) {
+        var $form = $(this);
+        var formData = {
+            username: $form.find('input[name="username"]').val(),
+            password: $form.find('input[name="password"]').val()
+        };
+
+        doLogin(formData);
+    });
 });
