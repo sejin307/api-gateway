ea3db2e76db04c257b459246598715efbe323965
sejin <sjkim@goodcen.com>
2023-09-14 17:47:40 +0900

    fileterName.getName() 변경

--- src/main/java/com/devops/api2/gateway/locator/provider/FilterListProvider.java
+++ src/main/java/com/devops/api2/gateway/locator/provider/FilterListProvider.java
@@ -6,6 +6,7 @@ import com.devops.api2.gateway.locator.definition.FilterDefinition;
 import com.devops.api2.gateway.locator.definition.BaseGatewayProperties;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +24,20 @@ public class FilterListProvider {
         this.baseGatewayProperties = baseGatewayProperties;
     }
 
+    /**
+     * Routing 에 대한 Filter  > 전체 CustomFilter를 공통으로 사용하지만. Route건마다 필요에따라 Filter 를 추가하여 구현해야함.
+     * @param customCircuitBreakerFilterFactory
+     * @param customFilter
+     * @param config
+     * @return
+     */
     public List<GatewayFilter> getFilters(CustomCircuitBreakerGatewayFilterFactory customCircuitBreakerFilterFactory, CustomFilter customFilter, CustomCircuitBreakerGatewayFilterFactory.Config config) {
         List<GatewayFilter> filters = new ArrayList<>();
         List<FilterDefinition> defaultFilters = baseGatewayProperties.getDefaultFilters();
 
         if (defaultFilters != null) {
             for (FilterDefinition filterName : defaultFilters){
-                if("CustomFilter".equals(filterName)){
+                if(StringUtils.hasText(filterName.getName())){
                     filters.add(customFilter.apply(new CustomFilter.Config()));
                 }
             }
